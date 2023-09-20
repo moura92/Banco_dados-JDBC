e3300bb2da5df3e7fae48565bc8a7e67756db59f
moura <alisson_moura_rj@hotmail.com>
2023-09-20 14:42:28 -0300

    Departamento e entidades

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,12 @@
+package aplicação;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "Books");
+		System.out.println(obj);
+	}
+}
